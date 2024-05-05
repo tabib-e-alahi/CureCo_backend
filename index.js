6318edaeffba389f1cac86dc31794ab25a14a294
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-05 23:55:09 +0600

    all products get method and a single product dynmic method added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require("dotenv").config();
 const app = express();
@@ -45,12 +45,13 @@ async function run() {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
 
-        const options = {
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, price: 1, service_id: 1, img: 1 },
-        };
+        // const options = {
+        //     // Include only the `title` and `imdb` fields in the returned document
+        //     projection: { title: 1, price: 1, service_id: 1, img: 1 },
+        // };
 
-        const result = await productCollection.findOne(query, options);
+        const result = await productCollection.findOne(query);
+        console.log(result)
         res.send(result);
     })
 
