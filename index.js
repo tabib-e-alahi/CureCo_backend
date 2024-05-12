e52af8e2369f3e8b2175878de3ba6181962b344e
tabib-e-alahi <etabib.alahi@gmail.com>
2024-05-12 13:15:35 +0600

    cart delte method added

--- index.js
+++ index.js
@@ -67,6 +67,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/productCarts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ============================== ===========================
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
